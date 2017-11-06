9fbb8946d32a794618249a438e186acc7d6c5418
Ruargh <ruargh@gmail.com>
2017-11-07 00:08:04 +0200

    bug: Update search shelf state

--- src/App.js
+++ src/App.js
@@ -12,12 +12,16 @@ class BooksApp extends React.Component {
     }
 
   state = {
-    books: []
+    books: [],
+    sbooks: [],
+    query: ''
   }
 
   componentDidMount() {
     this.refreshBooks()
+    this.updateSearchBooks(this.state.books)
   }
+
 
   updateBook(book, shelf) {
     BooksAPI.get(book).then( book => {
@@ -27,10 +31,30 @@ class BooksApp extends React.Component {
     })
 }
 
+updateQuery = (query) => {
+  this.setState({ query: query.trim() })
+  this.updateSearchBooks(this.state.books)
+}
+
+updateSearchBooks(myBooks) {
+  BooksAPI.search(this.state.query).then((sbooks) => {
+    //first reset all search result books shelf to none
+    sbooks.map( reset => reset.shelf = 'none' )
+    sbooks.map( sb =>
+      myBooks.map(mb =>
+        ( sb.id === mb.id ? sb.shelf = mb.shelf : '' )
+      )
+    )
+    this.setState({ sbooks })
+  })
+}
+
   refreshBooks() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
+      this.updateSearchBooks(books)
     })
+    
   }
 
   render() {
@@ -40,6 +64,9 @@ class BooksApp extends React.Component {
         <Route exact path="/search" render={() => (
           <SearchBooks
             books={this.state.books}
+            sbooks={this.state.sbooks}
+            query={this.state.query}
+            updateQuery={(query) => {this.updateQuery(query)}}
             onUpdateBook={(book, shelf) => {
               this.updateBook(book, shelf)
 
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import Book from './Book'
-import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends Component {
   constructor(props) {
@@ -9,65 +8,16 @@ class SearchBooks extends Component {
       this.handleBookUpdate = this.handleBookUpdate.bind(this)
   }
 
-  state = {
-    sbooks: [],
-    query: ''
-  }
-
-  updateQuery = (query, myBooks) => {
-    this.setState({ query: query.trim() })
-    this.refreshBooks(myBooks)
-  }
-
-  // Not needed anymore
-  // componentDidMount() {
-  //   this.refreshBooks()
-  // }
-
-
-/* Function to set book shelf state and display search results
- * TODO: should refactor into sub components
- * TODO: Try to reduce iterations required
- */
-  refreshBooks(myBooks) {
-    BooksAPI.search(this.state.query).then((sbooks) => {
-      //first reset all search result books shelf to none
-      sbooks.map( reset => reset.shelf = 'none' )
-      sbooks.map( sb =>
-        myBooks.map(mb =>
-          ( sb.id === mb.id ? sb.shelf = mb.shelf : '' )
-        )
-      )
-      this.setState({ sbooks })
-    })
+  updateQuery = (query) => {
+    this.props.updateQuery( query.trim() )
   }
 
   handleBookUpdate(book, shelf) {
     this.props.onUpdateBook(book, shelf)
-
-    BooksAPI.getAll().then((myBooks) => {
-
-    BooksAPI.search(this.state.query).then((sbooks) => {
-      //first reset all search result books shelf to none
-      sbooks.map( reset => reset.shelf = 'none' )
-      sbooks.map( sb =>
-        myBooks.map(mb =>
-          ( sb.id === mb.id ? sb.shelf = mb.shelf : '' )
-        )
-      )
-      this.setState({ sbooks })
-    })
-        
-    })
-    
   }
 
   render() {
 
-    const { books } = this.props
-    const { query } = this.state
-    let showBooks = this.state.sbooks
-    let myBooks = books
 
     return (
 
@@ -81,8 +31,8 @@ class SearchBooks extends Component {
             <input
               type="text"
               placeholder="Search by title or author"
-              value={query}
-              onChange={(event) => this.updateQuery(event.target.value, myBooks)}
+              value={this.props.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
                 />
 
           </div>
@@ -90,7 +40,7 @@ class SearchBooks extends Component {
         <div className="search-books-results">
 
           <Book
-            books={showBooks}
+            books={this.props.sbooks}
             onBookChange={this.handleBookUpdate}
           />
 
